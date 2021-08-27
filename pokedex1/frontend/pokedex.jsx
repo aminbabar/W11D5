15464ba750e1eb797f597a6907f728b56ddbe407
Amin Babar <amin.babar1@gmail.com>
2021-08-27 16:36:30 -0400

    thunk middleware added

--- pokedex1/frontend/pokedex.jsx
+++ pokedex1/frontend/pokedex.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {fetchAllPokemon} from "../frontend/util/api_util"
+import { receiveAllPokemon } from './actions/pokemon_actions';
+import configureStore from './store/store';
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
+
+
+    let store = configureStore()
+
+    window.receiveAllPokemon = receiveAllPokemon;
+    window.fetchAllPokemon = fetchAllPokemon;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch
+
     const rootEl = document.getElementById('root');
     ReactDOM.render(<h1>PokedexTest</h1>, rootEl);
 });
---
+++ pokedex1/frontend/reducers/entitiesReducer.js
@@ -0,0 +1,11 @@
+import {combineReducers} from "redux";
+
+import pokemonReducer from "./pokemon_reducer";
+
+
+const entitiesReducer = combineReducers({
+    pokemon: pokemonReducer,
+})
+
+
+export default entitiesReducer;
